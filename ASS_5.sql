2f5dc6e0b3df0d42d60842019efc3f224db25e98
saher hassaballa <158832472+SaherHassaballa@users.noreply.github.com>
2024-12-13 15:56:04 +0200

    ASS 5

--- ASS_5.sql
+++ ASS_5.sql
@@ -1,1 +1,39 @@
-select sum(employees.salary) ,  locations.city from employees, countries , departments , locations where locations.location_id = departments.location_id group by locations.city
+select * from employees
+/*---------------------------*/
+select employees.first_name ,employees.employee_id ,employees.department_id from employees , departments , locations
+
+where employees.department_id = departments.department_id 
+and locations.location_id = departments.location_id 
+and locations.city = 'Rome'
+/*-------------------------------------------*/
+
+select distinct employee_id from employees
+
+/*----------------------------------------------*/
+
+create table students( ID INT IDENTITY(1,1) primary key , 
+					   first_name varchar(50) not null ,
+					   last_name varchar(50) not null ,
+					   adress varchar(50) default 'N/A' ,
+					   Birth_date date );
+
+drop table students
+
+
+/*---------------------------------------------------*/
+
+INSERT INTO students (first_name, last_name , adress)
+VALUES ('Ahmed', 'Ali' , 'roma');
+
+
+update students 
+set adress = 'cairo'
+where first_name = 'Ahmed'
+
+/*----------------------------------------------------*/
+BEGIN TRANSACTION;
+
+Delete from students 
+where adress = 'cairo'
+
+ROLLBACK;
